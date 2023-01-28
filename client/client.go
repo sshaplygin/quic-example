604af41c54b08b97237a127fa47840b90a4d0f34
Saimon Shaplygin <semyuon@gmail.com>
2023-01-28 19:03:20 +0500

    update: code

--- client/client.go
+++ client/client.go
@@ -6,7 +6,6 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -15,47 +14,27 @@ import (
 	"github.com/lucas-clemente/quic-go/http3"
 )
 
-func GetRootCA(certPath string) *x509.CertPool {
-	caCertPath := path.Join(certPath, "ca.pem")
-	caCertRaw, err := ioutil.ReadFile(caCertPath)
-	if err != nil {
-		panic(err)
-	}
-	p, _ := pem.Decode(caCertRaw)
-	if p.Type != "CERTIFICATE" {
-		panic("expected a certificate")
-	}
-	caCert, err := x509.ParseCertificate(p.Bytes)
-	if err != nil {
-		panic(err)
-	}
-	certPool := x509.NewCertPool()
-	certPool.AddCert(caCert)
-	return certPool
-}
-
 func main() {
 	currentPath, err := os.Getwd()
-	// currentPath, err := os.Executable()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	log.Println(currentPath)
 	roundTripper := &http3.RoundTripper{
 		TLSClientConfig: &tls.Config{
-			RootCAs: GetRootCA(currentPath),
+			RootCAs: getRootCA(currentPath),
 		},
 	}
 	defer roundTripper.Close()
+
 	client := &http.Client{
 		Transport: roundTripper,
 	}
+
 	addr := "https://localhost:8080"
 	rsp, err := client.Get(addr)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-
 	defer rsp.Body.Close()
 
 	body := &bytes.Buffer{}
@@ -63,6 +42,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Body length: %d bytes", body.Len())
-	log.Printf("Response body %s", body.Bytes())
+
+	log.Printf("Body length: %d bytes \n", body.Len())
+	log.Printf("Response body %s \n", body.Bytes())
+}
+
+func getRootCA(certPath string) *x509.CertPool {
+	caCertPath := path.Join(certPath, "ca.pem")
+	caCertRaw, err := os.ReadFile(caCertPath)
+	if err != nil {
+		panic(err)
+	}
+
+	p, _ := pem.Decode(caCertRaw)
+	if p.Type != "CERTIFICATE" {
+		panic("expected a certificate")
+	}
+
+	caCert, err := x509.ParseCertificate(p.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(caCert)
+
+	return certPool
 }
--- server/main.go
+++ server/main.go
@@ -1,52 +1,43 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucas-clemente/quic-go/http3"
 	"log"
 	"net/http"
 	"os"
 	"path"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"github.com/lucas-clemente/quic-go"
-	"github.com/lucas-clemente/quic-go/http3"
-	"github.com/lucas-clemente/quic-go/quictrace"
 )
 
 func main() {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//logWriter, err := utils.GetQLOGWriter()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	//tracer := qlog.NewTracer(logWriter)
+	//conf := &quic_go.Config{
+	//	Tracer: tracer,
+	//}
 
 	router := gin.New()
 
 	router.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "OK"})
 	})
-	// _, filename, _, ok := runtime.Caller(0)
-	// if !ok {
-	// 	panic("Failed to get current frame")
-	// }
-	// currentPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	currentPath, err := os.Getwd()
-	// currentPath, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(currentPath)
-	tracer := quictrace.NewTracer()
-	quicConf := &quic.Config{QuicTracer: tracer}
 
 	server := http3.Server{
-		Server: &http.Server{
-			Addr:           ":8080",
-			Handler:        router,
-			ReadTimeout:    10 * time.Second,
-			WriteTimeout:   10 * time.Second,
-			MaxHeaderBytes: 1 << 20,
-		},
-		QuicConfig: quicConf,
+		//QuicConfig: conf,
+		Addr:    "127.0.0.1:8080",
+		Handler: router,
 	}
 
-	err = server.ListenAndServeTLS(path.Join(currentPath, "cert.pem"), path.Join(currentPath, "priv.key"))
-
+	err = server.ListenAndServeTLS(path.Join(currentPath, "cert.pem"), path.Join(currentPath, "private.key"))
 	if err != nil {
 		log.Printf("Server error: %v", err)
 	}
